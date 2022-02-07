26f876119f0b225f7770f3182eac967056c70e89
praveen.doranala <praveenkumarreddydoranala@gmail.com>
2022-02-08 00:21:45 +0530

    refactor:added one print statement on starting & added one comment

--- bikeshare.py
+++ bikeshare.py
@@ -10,14 +10,7 @@ sel_months=['all','january','february','march','aprail','may','june']
 sel_days=['all','monday','tuesday','wednesday','thursday','friday','saturday','sunday']
 
 def get_filters():
-    """
-    Asks user to specify a city, month, and day to analyze.
-
-    Returns:
-        (str) city - name of the city to analyze
-        (str) month - name of the month to filter by, or "all" to apply no month filter
-        (str) day - name of the day of week to filter by, or "all" to apply no day filter
-    """
+    print("welcome to udacity project")
     print('Hello! Let\'s explore some US bikeshare data!')
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     #print("\n Hello! select the city  \n 1.chicago 2.new york city 3.washington ")
@@ -52,6 +45,7 @@ def get_filters():
         
 
     print('-'*40)
+    #return date
     return city, month, day
     
 

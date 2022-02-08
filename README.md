cb873de5139bb73f18b6859e5e7705e009b10d65
praveen.doranala <praveenkumarreddydoranala@gmail.com>
2022-02-08 10:02:40 +0530

    docs:updated readme bikeshare

--- README.md
+++ README.md
@@ -1,17 +1,30 @@
 >**Note**: Please **fork** the current Udacity repository so that you will have a **remote** repository in **your** Github account. Clone the remote repository to your local machine. Later, as a part of the project "Post your Work on Github", you will push your proposed changes to the remote repository in your Github account.
 
 ### Date created
-Include the date you created this project and README file.
+feb/7/2022.
 
 ### Project Title
-Replace the Project Title
+bike share project
 
 ### Description
-Describe what your project is about and what it does
+in this project we need to use python libraries:pandas,numpy
 
 ### Files used
-Include the files used
+chicago.csv
+new_york_city.csv
+washington.csv
+
+
+
 
 ### Credits
 It's important to give proper credit. Add links to any repo that inspired you or blogposts you consulted.
+
+this is the project done by me through udacity
+thanks to udacity
+
+inthis project we will learn how to use data frames 
+numpy 
+pandAS
+
 
--- bikeshare.py
+++ bikeshare.py
@@ -1,6 +1,7 @@
 import time
 import pandas as pd
 import numpy as np
+import collections as col
 
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
@@ -10,14 +11,7 @@ sel_months=['all','january','february','march','aprail','may','june']
 sel_days=['all','monday','tuesday','wednesday','thursday','friday','saturday','sunday']
 
 def get_filters():
-    """
-    Asks user to specify a city, month, and day to analyze.
-
-    Returns:
-        (str) city - name of the city to analyze
-        (str) month - name of the month to filter by, or "all" to apply no month filter
-        (str) day - name of the day of week to filter by, or "all" to apply no day filter
-    """
+    print("welcome to udacity project")
     print('Hello! Let\'s explore some US bikeshare data!')
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     #print("\n Hello! select the city  \n 1.chicago 2.new york city 3.washington ")
@@ -52,6 +46,7 @@ def get_filters():
         
 
     print('-'*40)
+    #return date
     return city, month, day
     
 
